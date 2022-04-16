a2fcb0d4424f5a41f42f72c5d3957404d9c344de
Rauulito <7rnavarro7@gmail.com>
2022-04-16 19:00:44 +0200

    realizado

--- Conexion Cliente-Servidor.py
+++ Conexion Cliente-Servidor.py
@@ -24,8 +24,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
             temperatura_minima = float(input("Temperatura minima:"))
             temperatura_maxima = float(input("Temperatura máxima:"))
             presion = float(input("Presión:"))
-            plumiometría = float(input("Pluviometría:"))
+            pluviometría = float(input("Pluviometría:"))
+
             else:
                 print('[*] Datos recibidos: {}'.format(data.decode('utf-8')))
             conn.send(data) # Hacemos echo convirtiendo de nuevo a bytes
 
+#iniciamos al cliente
+# El cliente debe tener las mismas especificaciones del servidor
+host = socket.gethostname()
+port = 12345
+BUFFER_SIZE = 1024
+MESSAGE = ["La temperatura minima es:" +temperatura_minima,"La temperatura maxima es:" +temperatura_maxima,"La presion es:" +presion,"La pluviometria es:" +pluviometría] # Datos que queremos enviar(Datos recibidos)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
+    socket_tcp.connect((host, port))
+    # Convertimos str a bytes
+    socket_tcp.send(MESSAGE.encode('utf-8'))
+    data = socket_tcp.recv(BUFFER_SIZE)
